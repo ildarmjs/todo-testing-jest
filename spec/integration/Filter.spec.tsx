0ee2358b1be8dfc6ed279354f872f004eef42bba
Ildar <ildar.1795@gmail.com>
2024-09-22 19:14:00 +0400

    feat: added new integration test

--- spec/integration/Filter.spec.tsx
+++ spec/integration/Filter.spec.tsx
@@ -1,9 +1,67 @@
+import { render, screen } from '@testing-library/react'
+import { App } from 'src/App'
+import ue from '@testing-library/user-event'
+
+const userEvent = ue.setup({
+	advanceTimers: jest.advanceTimersByTime
+})
+
 describe('Список задач', () => {
-    // не содержит выполненные задачи
-    // после нажатия на кнопку фильтрации
-    it.todo('с включенным фильтром');
-    
-    // показывает как выполненные, так и не выполненные задачи
-    // после повторного нажатия на кнопку фильтрации
-    it.todo('с выключенным фильтром');
-});
+	it('пустой, и кнопка фильтра отсутствует', async () => {
+		render(<App />)
+
+		const filterBtn = screen.queryByTestId('filter-button')
+		const items = screen.queryByRole('list')
+
+		expect(items).not.toBeInTheDocument()
+		expect(filterBtn).not.toBeInTheDocument()
+	})
+
+	it('с включенным фильтром', async () => {
+		render(<App />)
+
+		const inputEl = screen.getByRole('textbox')
+		const addBtnEl = screen.getByAltText(/Добавить/i)
+
+		await userEvent.clear(inputEl)
+		await userEvent.type(inputEl, 'Первый заголовок')
+		await userEvent.click(addBtnEl)
+
+		await userEvent.type(inputEl, 'Второй заголовок')
+		await userEvent.click(addBtnEl)
+
+		const firstElem = screen.getByRole('checkbox', {
+			name: /Первый заголовок/i
+		})
+		const secondElem = screen.getByRole('checkbox', {
+			name: /Второй заголовок/i
+		})
+
+		await userEvent.click(firstElem)
+
+		const filterBtn = screen.getByTestId('filter-button')
+
+		await userEvent.click(filterBtn)
+
+		const items = screen.getAllByRole('listitem')
+
+		expect(filterBtn).toHaveClass('active')
+
+		expect(items).toHaveLength(1)
+
+		expect(secondElem).not.toBeInTheDocument()
+	})
+
+	it('с выключенным фильтром', async () => {
+		render(<App />)
+
+		const filterBtn = screen.getByTestId('filter-button')
+
+		await userEvent.click(filterBtn)
+
+		const items = screen.getAllByRole('listitem')
+
+		expect(filterBtn).not.toHaveClass('active')
+		expect(items).toHaveLength(2)
+	})
+})
--- spec/integration/Notifier.spec.tsx
+++ spec/integration/Notifier.spec.tsx
@@ -1,7 +1,31 @@
-import { render, screen } from "@testing-library/react";
-import { Notifier } from "src/components/Notifier";
+import { render, screen } from '@testing-library/react'
+import { Notifier } from 'src/components/Notifier'
+import * as taskSliceModule from 'src/store/taskSlice'
+import { JestStoreProvider } from '../utils/JestStoreProvider'
 
-describe('Оповещение при вополнении задачи', () => {
-    it.todo('появляется и содержит заголовок задачи');
-    it.todo('одновременно может отображаться только одно');
-});
+describe('Оповещение при выполнении задачи', () => {
+	const notification = 'Задача выполнена'
+	const spied = jest
+		.spyOn(taskSliceModule, 'getNotification')
+		.mockReturnValue(notification)
+	const onClose = jest.fn()
+
+	it('появляется и содержит заголовок задачи', async () => {
+		render(<Notifier open={true} onClose={onClose} task={notification} />, {
+			wrapper: JestStoreProvider
+		})
+
+		jest.runAllTimers()
+		const notifierEl = screen.getByText(notification)
+
+		expect(notifierEl).toBeInTheDocument()
+		expect(notifierEl.textContent).toBe(notification)
+	})
+	it('одновременно может отображаться только одно', () => {
+		render(<Notifier open={true} onClose={onClose} task={notification} />, {
+			wrapper: JestStoreProvider
+		})
+		const notifierElements = screen.getAllByText(notification)
+		expect(notifierElements.length).toBe(1)
+	})
+})
